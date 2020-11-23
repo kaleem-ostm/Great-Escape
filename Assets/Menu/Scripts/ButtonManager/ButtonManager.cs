99da707f40d08bd63080094cd4470c0dd99e6ce5
kaleem-ostm <74930796+kaleem-ostm@users.noreply.github.com>
2020-11-23 19:59:30 +0000

    Update 1

---
+++ Assets/Menu/Scripts/ButtonManager/ButtonManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    
+    public void PlayGameBtn()
+    {
+        SceneManager.LoadScene(1); // When the player hits the Play Button, it will go to the level scene.
+    }
+
+    public void OptionsBtn()
+    {
+        SceneManager.LoadScene(2); // Redirects the player to the options screen.
+    }
+
+    public void QuitGameBtn()
+    {
+        Application.Quit(); // Exits the game. You can't hit the quit button while in Unity Engine. 
+    }
+
+}
